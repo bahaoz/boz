4fcad0c11e674978618eb07e7deb298f03e9c287
bahaoz <bahaoz.bo@gmail.com>
2026-01-11 17:08:48 +0300

    Rename plugin for v2 branding

--- melontik-profit-stock.php
+++ melontik-profit-stock.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: Melontik Karlılık + Ortak Stok
- * Description: WooCommerce için kârlılık raporları, ürün maliyet alanları ve ileride pazaryeri/ortak stok altyapısı sağlar.
- * Version: 1.0.0
- * Author: Melontik
+ * Plugin Name: Maliyet Yönetim Sistemi
+ * Description: WooCommerce için kârlılık raporları, ürün maliyet alanları ve pazaryeri altyapısı sağlar.
+ * Version: 2.0.0
+ * Author: Barugu
  * Text Domain: melontik-profit-stock
  * Requires at least: 6.0
  * Requires PHP: 7.4
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'MPS_VERSION', '1.0.0' );
+define( 'MPS_VERSION', '2.0.0' );
 define( 'MPS_PLUGIN_FILE', __FILE__ );
 define( 'MPS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MPS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
